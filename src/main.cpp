d65b0233cfe2f5ee289b57f033eddc27fb913e77
meltingmodem <jukic.rok@gmail.com>
2026-09-22 16:26:52 +0200

    simple POC for masquerading as a mass storage device

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <USB.h>
+#include <USBMSC.h>
+
+USBMSC MSC;
+
+static int32_t onWrite(uint32_t lba, uint32_t offset, uint8_t* buffer, uint32_t bufsize) {
+  // Write bufsize bytes from buffer to your storage at block lba
+  return bufsize;
+}
+
+static int32_t onRead(uint32_t lba, uint32_t offset, void* buffer, uint32_t bufsize) {
+  // Read bufsize bytes into buffer from storage at block lba
+  char array[bufsize] = { 0x41 };
+  array[bufsize-1] = 0;
+  buffer = array;
+  return bufsize;
+}
+
+static bool onStartStop(uint8_t power_condition, bool start, bool load_eject) {
+  return true;
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    delay(10000);
+    Serial.begin(9600);
+    
+    USB.begin();
+    MSC.vendorID("ESP32");
+    MSC.productID("USB_MSC");
+    MSC.productRevision("1.0");
+    MSC.onRead(onRead);
+    MSC.onWrite(onWrite);
+    MSC.onStartStop(onStartStop);
+    MSC.mediaPresent(true);
+    MSC.begin(100, 4096);
+
+    Serial.println("Setup phase completed");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    delay(1000);
 }
